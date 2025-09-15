42fd0e4f3406d2c4bd6601c4d559fd4360eb1016
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-09-15 10:21:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,13 +17,13 @@ let historiaFinal = "";
 botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo() {
-  atual = 0;
-  historiaFinal = "";
-  telaInicial.style.display = 'none';
-  caixaPerguntas.classList.remove(".mostrar");
-  caixaAlternativas.classList.remove(".mostrar");
-  caixaResultado.classList.remove(".mostrar");
-  mostraPergunta();
+   atual = 0;
+   historiaFinal = "";
+   telaInicial.style.display = 'none';
+   caixaPerguntas.classList.remove(".mostrar");
+   caixaAlternativas.classList.remove(".mostrar");
+   caixaResultado.classList.remove(".mostrar");
+   mostraPergunta();
 }
 
 function mostraPergunta() {
