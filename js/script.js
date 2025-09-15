40ee8c4ae29a6f24d10439c5935e02a5c6e7e23e
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-09-15 10:20:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ botaoIniciar.addEventListener('click', iniciaJogo);
 
 function iniciaJogo() {
   atual = 0;
+  historiaFinal = "";
   telaInicial.style.display = 'none';
   caixaPerguntas.classList.remove(".mostrar");
   caixaAlternativas.classList.remove(".mostrar");
