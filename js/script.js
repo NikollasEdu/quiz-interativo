76f1d2ff7550433ddbecf2f881b315279139e3cd
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-09-23 10:56:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,105 @@
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            {
+                texto: "nike",
+                afirmacao: [
+                    "você tem bom gosto",
+                    "afirmação 2"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "adidas",
+                afirmacao: [
+                    "você é vaiado",
+                    "você não sabe escolher"
+                ],
+                proxima: 1,
+            }
+         ]
+     },
+     {
+         enunciado: "pergunta 2",
+         alternativas: [
+             {
+                 texto: "nike",
+                 afirmacao: [
+                     "você tem bom gosto",
+                     "afirmação 2"
+                 ],
+                 proxima: 2,
+            },
+            {
+                 texto: "adidas",
+                 afirmacao: [
+                     "você é vaiado",
+                     "você não sabe escolher"
+                 ],
+                proxima: 2,
+      }
+    ]
+  },
+  {
+    enunciado: "pergunta 4",
+    alternativas: [
+      {
+        texto: "nike",
+        afirmacao: [
+          "você tem bom gosto",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "adidas",
+        afirmacao: [
+          "você é vaiado",
+          "você não sabe escolher"
+        ],
+      }
+    ]
+  },
+  {
+    enunciado: "Você prefere nike ou adidas?",
+    alternativas: [
+      {
+        texto: "nike",
+        afirmacao: [
+          "você tem bom gosto",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "adidas",
+        afirmacao: [
+          "você é vaiado",
+          "você não sabe escolher"
+        ],
+      }
+    ]
+  },
+  {
+    enunciado: "pergunta 5",
+    alternativas: [
+      {
+        texto: "nike",
+        afirmacao: [
+          "você tem bom gosto",
+          "afirmação 2"
+        ],
+      },
+      {
+        texto: "adidas",
+        afirmacao: [
+          "você é vaiado",
+          "você não sabe escolher"
+        ],
+      }
+    ]
+  }
+]
+
 import {aleatorio, nome} from './aleatorio.js';
 import {perguntas} from './perguntas.js';
 
